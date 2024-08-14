e4cf981a942f88f077a9644c9e52379036a86845
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-14 07:46:34 -0400

    arithmetic operators

---
+++ basics/arithmetic_operators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+    // Order of operations
+    // 1. Parenthesis
+    // 2. * % /
+    // 3. + & -
+
+    int students = 6 - (5 + 4) * 3 / 2;
+
+    std::cout << students;
+
+    // int students = 20;
+    // students = students + 1;
+    // students += 1;
+    // students++;
+
+    // students--;
+
+    // students = students * 2;
+    // students *= 2;
+
+    // students = students / 2;
+    // students /= 2;
+    
+    // int remainder = students % 2;
+
+    // std::cout << remainder;
+
+    return 0;
+}
