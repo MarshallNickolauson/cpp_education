9a8c358a93acf7fc1a9fe03930195d887421007d
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-16 06:52:22 -0400

    finished binary search

--- programs/binary_search.cpp
+++ programs/binary_search.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <ctime>
+#include <chrono>
 
 using std::cout;
 
+void binary_search(long long low, long long high, long long numToGuess, int tries) { 
+    cout << "Low: " << low << '\n';
+    cout << "High: " << high << '\n';
+    cout << "Number to Guess: " << numToGuess << '\n';
+    cout << "Tries: " << tries << '\n';
+
+    long long guess = low + (high - low) / 2;
+    cout << "Guess: " << guess << '\n';
+    if(guess < numToGuess) {
+        binary_search(guess + 1, high, numToGuess, tries + 1);
+    } else if (guess > numToGuess) {
+        binary_search(low, guess - 1, numToGuess, tries + 1);
+    } else {
+        cout << "Guessed!" << '\n';
+        cout << guess << '\n';
+    }
+}
+
 int main() {
 
-    int num;
-    int guess;
-    int tries;
+    /*
+        Number to Guess: 5678944573567894
+        Tries: 57
+        Guess: 5678944573567894
+        Guessed!
+        5678944573567894
+        Time taken: 0.0188366 seconds - BRO WHAT
+    */
 
-    int range = 1000;
+    long long numToGuess;
+    long long range = 1000000000000000000LL;
 
     srand(time(NULL));
-    num = (rand() % range) + 1;
+    // numToGuess = (rand() % range) + 1;
+    numToGuess = 5678944573567894;
 
-    cout << "##### Random number set to: " << num << '\n';
+    auto start = std::chrono::high_resolution_clock::now();
 
-    guess = (rand() % range) + 1;
-    if (guess > num) {
-        cout << "Guess was " << guess << ". Go lower.";
-    } else if (guess < num) {
-        cout << "Guess was " << guess << ". Go higher.";
-    } else {
-        cout << "Guessed it right!";
-    }
+    cout << "##### Random number set to: " << numToGuess << '\n';
 
-    // The above works. Finish when you learn about functions so you can call it recursively.
+    binary_search(0, range, numToGuess, 1);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+
+    cout << "Time taken: " << duration.count() << " seconds\n";
 
     return 0;
 }
