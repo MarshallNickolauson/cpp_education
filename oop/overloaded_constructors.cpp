4dafb5838495674028994dbe34be94160e744a6a
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-18 15:05:50 -0400

    overloaded constructors

---
+++ oop/overloaded_constructors.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Pizza {
+    public:
+        std::string topping1;
+        std::string topping2;
+
+    Pizza() {}
+    Pizza(std::string topping1) {
+        this->topping1 = topping1;
+    }
+    Pizza(std::string topping1, std::string topping2) {
+        this->topping1 = topping1;
+        this->topping2 = topping2;
+    }
+};
+
+int main() {
+
+    Pizza pizza1("pepperoni");
+    Pizza pizza2("pepperoni", "mushrooms");
+    Pizza pizza3;
+
+    return 0;
+}
