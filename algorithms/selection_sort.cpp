df423f79a3013324aa9086a2df9f473128f5dee6
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-19 06:44:53 -0400

    selection sort

--- algorithms/selection_sort.cpp
+++ algorithms/selection_sort.cpp
@@ -31,10 +31,41 @@ int main() {
         p_array[i] = (rand() % size) + 1;
     }
 
-    int swapCounter = 0;
+    long long swapCounter = 0;
 
     int *p_temp = nullptr;
     p_temp = new int;
+
+    auto start1 = std::chrono::high_resolution_clock::now();
+
+    /*
+
+    Swapping every single one:
+        int size = 100000;
+        2497835211 swaps
+        15.5282 seconds
+
+    */
+    // for(int i = 0; i < size - 1; i++) {
+    //     int min_index = i;
+    //     for (int j = i+1; j < size; j++) {
+    //         if (p_array[j] < p_array[min_index]) {
+    //             *p_temp = p_array[i];
+    //             p_array[i] = p_array[min_index];
+    //             p_array[min_index] = *p_temp;
+    //             swapCounter++;
+    //         }
+    //     }
+    // }
+
+    /*
+
+    Swapping only minimum index:
+        int size = 100000;
+        99999 swaps
+        3.48438 seconds
+
+    */
 
     for(int i = 0; i < size - 1; i++) {
         int min_index = i;
@@ -50,15 +81,14 @@ int main() {
         swapCounter++;
     }
 
+    auto end1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration1 = end1 - start1;
+
     delete p_temp;
     delete[] p_array;
 
-    // for(int i = 0; i < size; i++) {
-    //     cout << p_array[i] << ' ';
-    // }
-    // cout << '\n';
-
     cout << swapCounter << " swaps" << endl;
+    cout << duration1.count() << " seconds" << endl;
 
     return 0;
 }
