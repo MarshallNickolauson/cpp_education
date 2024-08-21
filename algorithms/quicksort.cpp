0721acf0d100501ad514e3afdec46ce75087761e
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-21 06:44:10 -0400

    optimized quicksort

--- algorithms/quicksort.cpp
+++ algorithms/quicksort.cpp
@@ -3,28 +3,22 @@
 #include <chrono>
 #include <iomanip>
 #include <cstdlib>
+#include <vector>
 
-// UNOPTIMIZED - Stack overflow past 9999 elements
-// Quicksort for 9999 elements: 0.10278 seconds 
+// Quicksort for 100001 elements: 0.0086013 seconds 
 
-int partition(int array[], int start, int end);
-void quicksort(int array[], int start, int end);
+long long partition(std::vector<long long>& array, long long start, long long end);
+void quicksort(std::vector<long long>& array, long long start, long long end);
 
 int main() {
 
     srand(time(NULL));
 
-    int size = 9999;
-    int array[size];
+    long long size = 100001;
+    std::vector<long long> array(size);
 
-    // Fill the first half with random numbers
-    for(int i = 0; i < size / 2; i++) {
-        array[i] = (rand() % (size / 2)) + 1;
-    }
-
-    // Fill the second half with sequential numbers
-    for(int i = size / 2; i < size; i++) {
-        array[i] = i + 1;
+    for(long long i = 0; i < size; i++) {
+        array[i] = (rand() % (size)) + 1;
     }
 
     auto start1 = std::chrono::high_resolution_clock::now();
@@ -39,10 +33,10 @@ int main() {
     return 0;
 }
 
-int partition(int array[], int start, int end) {
-    int pivot = array[end];
-    int x = start - 1;
-    for(int i = start; i < end; i++) {
+long long partition(std::vector<long long>& array, long long start, long long end) {
+    long long pivot = array[end];
+    long long x = start - 1;
+    for(long long i = start; i < end; i++) {
         if(array[i] < pivot) {
             x++;
             std::swap(array[i], array[x]);
@@ -53,9 +47,9 @@ int partition(int array[], int start, int end) {
     return x + 1;
 }
 
-void quicksort(int array[], int start, int end) {
+void quicksort(std::vector<long long>& array, long long start, long long end) {
     if(start < end) {
-        int p = partition(array, start, end);
+        long long p = partition(array, start, end);
         quicksort(array, start, p - 1);
         quicksort(array, p + 1, end);
     }
