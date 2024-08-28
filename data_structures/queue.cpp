b7f7962cb0ae9c0fd9c996ffc91ae89c9f0fdf66
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-08-27 21:39:39 -0400

    queue study

--- data_structures/queue.cpp
+++ data_structures/queue.cpp
@@ -32,33 +32,27 @@ public:
     //enqueue
     void enqueue(int value) {
         Node* newNode = new Node(value);
+        newNode->prev = tail;
+        if (tail != nullptr) {
+            tail->next = newNode;
+        }
         if (head == nullptr) {
-            head = tail = newNode;
-        } else {
-            newNode->next = head;
-            head->prev = newNode;
             head = newNode;
         }
+        tail = newNode;
     }
 
     //dequeue
-    Node* dequeue() {
-        if (tail != nullptr) {
-            Node* nodeToReturn = tail;
-
-            if (tail->prev != nullptr) { // more than 1 node in the queue
-                tail = tail->prev;
-                tail->next = nullptr;
-            } else { // if ony one node is in the queue
-                head = tail = nullptr;
-            }
-
-            nodeToReturn->prev = nullptr; // disconnect the dequeued node
-            return nodeToReturn;
-        } else {
-            cout << "Nothing in the queue" << endl;
-            return nullptr;
+    int dequeue() {
+        if (head != nullptr) {
+            Node* nodeToReturn = head;
+            head = head->next;
+            head->prev = nullptr;
+            int valueToReturn = nodeToReturn->data;
+            delete nodeToReturn;
+            return valueToReturn;
         }
+        return 0;
     }
 
     //print
@@ -83,9 +77,9 @@ int main() {
 
     queue->print();
 
-    Node* dequeued1 = queue->dequeue();
+    int dequeued1 = queue->dequeue();
 
-    delete dequeued1;
+    cout << dequeued1 << endl;
 
     queue->print();
 
